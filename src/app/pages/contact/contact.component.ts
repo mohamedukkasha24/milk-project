7cf590f6cefe0ba9966d36def1c74879bf0d7a62
mohamedukkasha24 <mohamedukkasha24@gmail.com>
2025-08-30 13:20:59 +0530

    updated

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -4,11 +4,13 @@ import {
   FormGroup,
   Validators,
   ReactiveFormsModule,
+  FormsModule,
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import emailjs from '@emailjs/browser';
 @Component({
   selector: 'app-contact',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
@@ -30,6 +32,34 @@ export class ContactComponent {
       this.contactForm.reset();
     } else {
       this.contactForm.markAllAsTouched();
+    }
+  }
+
+  sendEmail(form: any) {
+    if (form.valid) {
+      emailjs
+        .send(
+          'service_g31enas', // 🔹 Replace with your EmailJS Service ID
+          'template_75bo5i6', // 🔹 Replace with your EmailJS Template ID
+          {
+            from_name: form.value.from_name,
+            reply_to: form.value.reply_to,
+            subject: form.value.subject,
+            message: form.value.message,
+          },
+          'C710QmoAQpHRQSJ1r' // 🔹 Replace with your EmailJS Public Key
+        )
+        .then(
+          (response) => {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('✅ Your message has been sent successfully!');
+            form.reset(); // clear form after send
+          },
+          (err) => {
+            console.error('FAILED...', err);
+            alert('❌ Failed to send message. Please try again.');
+          }
+        );
     }
   }
 }
